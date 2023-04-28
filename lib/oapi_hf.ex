62a44deaa357d9f897cd3e19f3d21c92e4033b41
Feynman Liang <feynman.liang@gmail.com>
2023-04-28 17:45:54 +0000

    Init mix project

---
+++ lib/oapi_hf.ex
@@ -0,0 +1,18 @@
+defmodule OapiHf do
+  @moduledoc """
+  Documentation for `OapiHf`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OapiHf.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oapi_hf_test.exs
@@ -0,0 +1,8 @@
+defmodule OapiHfTest do
+  use ExUnit.Case
+  doctest OapiHf
+
+  test "greets the world" do
+    assert OapiHf.hello() == :world
+  end
+end
