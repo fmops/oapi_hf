9d132c02b4c5b62e30c1e7b2b452a5bf1b04d86f
Feynman Liang <feynman.liang@gmail.com>
2023-05-30 16:37:17 +0000

    Remove manual rename

--- config/config.exs
+++ config/config.exs
@@ -4,10 +4,7 @@ if Mix.env() == :dev do
   config :oapi_generator,
     default: [
       base_location: "lib/text_generation_inference",
-      base_module: TextGenerationInference,
-      rename: [
-        {"Text Generation Inference", "TextGenerationInference{}"}
-      ]
+      base_module: TextGenerationInference
     ]
 end
 
